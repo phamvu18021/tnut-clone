25dabfcaf4fcae6e8cbd8eed506c91c0ab0d5fba
phamvu18021 <phamvu12800123@gmail.com>
2023-11-09 09:06:04 +0700

    aa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,14 +13,6 @@ export default function Document() {
         />
       </Head>
       <body>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXW3K2P"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `,
-          }}
-        />
         <Main />
         <NextScript />
       </body>
