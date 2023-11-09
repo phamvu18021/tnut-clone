8c89257ac790e64b8160b6fa0e863a97f45e1342
phamvu18021 <phamvu12800123@gmail.com>
2023-11-09 09:29:16 +0700

    lasst

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["tnut.vn"], // Add the domains you want to allow here
+    domains: ["bcvt.edu.vn"], // Add the domains you want to allow here
     //  domains: ["localhost"],
   },
 };
