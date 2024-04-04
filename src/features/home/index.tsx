aa84ff96eabe31c74c4ab463ff9b0b50ae878c08
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-04-04 08:46:03 +0700

    aaa

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -57,10 +57,6 @@ const LastestPost = dynamic(
 
 export const Home = () => {
   const { isOpen, onOpen } = useModal();
-
-  const [news, setNews] = useState<any[]>([]);
-  const [notifis, setNotifis] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
   const [home_content, setHomeContent] = useState<any>(null);
 
   useEffect(() => {
@@ -74,54 +70,14 @@ export const Home = () => {
       } catch (error) {
         console.log(error);
       }
-      setIsLoading(false);
     };
     getHomeContent();
   }, []);
 
   useEffect(() => {
-    const getPosts = async () => {
-      setIsLoading(true);
-      try {
-        const res = await fetch(`/api/posts/?type=news&page=1`, {
-          next: { revalidate: 3 }
-        });
-
-        const data: { posts: any[]; totalPosts: string } = await res.json();
-        const { posts } = data;
-        posts?.length && setNews([posts[0], posts[1], posts[2], posts[4]]);
-      } catch (error) {
-        console.log(error);
-      }
-      setIsLoading(false);
-    };
-
-    getPosts();
-  }, []);
-
-  useEffect(() => {
-    const getPosts = async () => {
-      setIsLoading(true);
-      try {
-        const res = await fetch(`/api/posts/?type=notifis&page=1`, {
-          next: { revalidate: 3 }
-        });
-
-        const data: { posts: any[]; totalPosts: string } = await res.json();
-        const { posts } = data;
-        posts?.length && setNotifis([posts[0], posts[1], posts[2], posts[4]]);
-      } catch (error) {
-        console.log(error);
-      }
-      setIsLoading(false);
-    };
-
-    getPosts();
-  }, []);
-  useEffect(() => {
     const timeout = setTimeout(() => {
       if (!isOpen && onOpen) onOpen();
-    }, 6000);
+    }, 8000);
 
     return () => window.clearTimeout(timeout);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -136,7 +92,7 @@ export const Home = () => {
       <Majors majors={home_content?.acf?.nganh_dao_tao} />
       <Testimonials testimonials={home_content?.acf?.danh_gia_cua_hoc_vien} />
       <Advertisement advertisement={home_content?.acf?.quang_cao} />
-      <Event news={news} notifis={notifis} />
+      <Event />
       <Circulars circulars={home_content?.acf?.thong_tu} />
     </>
   );
