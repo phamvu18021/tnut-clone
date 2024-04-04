29ba19c9112b632d060bbf0575229563f57ab6e7
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-04-04 08:51:16 +0700

    za

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -77,7 +77,7 @@ export const Home = () => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       if (!isOpen && onOpen) onOpen();
-    }, 8000);
+    }, 3000);
 
     return () => window.clearTimeout(timeout);
     // eslint-disable-next-line react-hooks/exhaustive-deps
