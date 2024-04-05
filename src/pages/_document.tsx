6ef198cbc523059a911c423640d419fc21de07b8
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-04-05 14:18:55 +0700

    tagmanager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import { Head, Html, Main, NextScript } from "next/document";
 import Script from "next/script";
 
@@ -7,7 +8,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        {/* <Script id="google-tag-manager" strategy="afterInteractive">
           {`
              setTimeout(function() {(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -15,7 +16,7 @@ export default function Document() {
              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
              })(window,document,'script','dataLayer','GTM-TDCV4KK')}, 8000);            
           `}
-        </Script>
+        </Script> */}
         {/* <link
           rel="preload"
           href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
@@ -46,8 +47,27 @@ export default function Document() {
             `
           }}
         />
+
         <Main />
         <NextScript />
+        <script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{
+            __html: `
+                setTimeout(function() {
+                  (function(w,d,s,l,i){
+                    w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});
+                    var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                    j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                    f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-TDCV4KK')
+                }, 8000);
+              `
+          }}
+        />
       </body>
     </Html>
   );
