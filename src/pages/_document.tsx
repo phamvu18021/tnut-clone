7be3ec8eae83f86f752e377aadfb388cb0ce07e3
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-04-05 14:09:23 +0700

    font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,17 +16,17 @@ export default function Document() {
              })(window,document,'script','dataLayer','GTM-TDCV4KK')}, 8000);            
           `}
         </Script>
-        <link
+        {/* <link
           rel="preload"
           href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           as="style"
+        /> */}
+        {/* <noscript> */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         />
-        <noscript>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          />
-        </noscript>
+        {/* </noscript> */}
       </Head>
       <body>
         <noscript>
