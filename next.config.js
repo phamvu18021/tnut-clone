003f63fc9c8c9d3db973b76759d1880fe5b61b6d
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-04-06 08:26:57 +0700

    config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["nologin.tnut.vn", "localhost"] // Add the domains you want to allow here
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/robots.txt",
-        destination: "/api/robots"
-      }
-    ];
   }
 };
 
