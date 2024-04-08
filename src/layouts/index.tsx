a9cc44ba5e009e4d0b1ac7b151f8dcf900553e1b
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-04-08 15:35:26 +0700

    dynamic header+footer

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,10 +1,18 @@
 import { ReactNode } from "react";
 import { CTA } from "./components/Cta";
-import { Footer } from "./footer";
-import { Header } from "./header";
+import dynamic from "next/dynamic";
+import { Loading } from "@/components/Loading";
 interface ILayout {
   children: ReactNode;
 }
+
+const Header = dynamic(() => import("./header").then((mod) => mod.Header), {
+  loading: () => <Loading />
+});
+
+const Footer = dynamic(() => import("./footer").then((mod) => mod.Footer), {
+  loading: () => <Loading />
+});
 const Layout = ({ children }: ILayout) => {
   return (
     <>
