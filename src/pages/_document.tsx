98711f7d45e97f239a2101a6755bba3b9deab782
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-05-15 10:29:10 +0700

    funt

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,19 +35,18 @@ export default function Document() {
           id="google-tag-manager"
           dangerouslySetInnerHTML={{
             __html: `             
-            function timeOut() {
-              timeoutID = setTimeout(function() {
-                (function(w,d,s,l,i){
-                  w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});
-                  var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                  j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                  f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-TDCV4KK');
-              }, 8000);
-            }
+            var timeoutID =() => setTimeout(function() {
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TDCV4KK')
+            }, 8000);
+            timeoutID();
             setTimeout(
               function() {
                     clearTimeout(timeoutID);
