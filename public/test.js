a8f63cc76224e868e94624ffeea5d019a834d894
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-07-26 16:47:16 +0700

    Update test.js

--- public/test.js
+++ public/test.js
@@ -1,13 +1,14 @@
 function GetForm (name, id) {
     const f = document.createElement("iframe");
     let class_form = document.getElementsByClassName('formio_form_iframe_container')
+    console.log(class_form)
     let i = 0
     let odoo_utm = th_get_cookie()
     let referrer = document.referrer
     f.setAttribute("src", name + "?referrer=" + odoo_utm);
     f.style.width = "100%";
     f.classList.add("formio_form_embed")
-
+    
     for (; i < class_form.length; i++) {
         let new_id = "formio_form_iframe_container_" + id + '_' + i;
         class_form[i].id = new_id
