740e8b3a926c5a1a2cd20705b0ea484ea0154d32
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-12-02 11:30:39 +0700

    update to main

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["nologin.tnut.vn"] // Add the domains you want to allow here
+    domains: ["nologin.tnut.vn", "10.10.51.16"] // Add the domains you want to allow here
   }
 };
 
